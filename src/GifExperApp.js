499d86e8abacd00f9c8d395be3376fe496616a15
Santiago Lopez <santiagoloepz@gmail.com>
2020-09-21 17:39:35 -0500

    1.agregar algo aun arreglo

--- src/GifExperApp.js
+++ src/GifExperApp.js
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const GifExperApp = () => {
+
+    //const categories = ['One Punch','Naruto','JOJO'];
+   const [categories, setCategories] = useState(['One Punch','Naruto','JOJO']);
+   
+   const handleAdd  =()=>{
+       // forma de añadir algo con useStarw
+       const add='HunterXHunter'
+       setCategories([...categories,add]);     
+   }
+
+
     return (
         <>
             <h2>GifExperApp</h2>
             <hr/>
+
+            <button onClick={handleAdd}>Agregar</button>
+
+            <ol>
+                {
+                    categories.map((catgory ) =>{
+                    return <li key={catgory}>{catgory}</li>
+                    })
+                }
+            </ol>
         </>
     );
 }
