946710f30481d1dc35224ffac57ae23d0124d07a
Santiago Lopez <santiagoloepz@gmail.com>
2020-09-23 17:05:21 -0500

    2.correcion de parametros obligatorios y su respectivo tests

--- src/tests/GifGridItem.test.js
+++ src/tests/GifGridItem.test.js
@@ -4,10 +4,14 @@ import React from "react";
 import { GifGridItem } from '../components/GifGridItem';
 
 describe('Pruebas <GifGridItem/>', () => {
+
+    const title = 'un titulo'
+    const url = 'https://localhost/algo.jpg'
+
     test('debe de mostrar <GifGridItem/> correctamente', () => {
-        const wrapper = shallow(<GifGridItem/>);
+        const wrapper = shallow(<GifGridItem title={title} url={url}/>);
+                                         
         expect(wrapper).toMatchSnapshot();
-        
     });
     
 });
