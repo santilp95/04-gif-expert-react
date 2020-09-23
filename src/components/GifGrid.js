bd389122eb43318b330a5ed47a26b1f78f0453eb
Santiago Lopez <santiagoloepz@gmail.com>
2020-09-22 19:50:08 -0500

    6.1 llamar los elementos en el html

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,18 +1,19 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export const GifGrid = ({category}) => {
 
-    const [count, setCount] = useState(0);
+    const [images, setImages] = useState([])
 
     // esto funciona para ejecutar cuando el compnente se rendiriza por primera vez si se manda el array vacio
     useEffect(() => {
         getGifs();
     },[])
         
+    
 
     const  getGifs = async() =>{
 
-        const url = `https://api.giphy.com/v1/gifs/search?q=JOJO&limit=10&api_key=qsUmBiU0pSR30iCqtxxSlrEeVjYfJAGP`;
+        const url = `https://api.giphy.com/v1/gifs/search?q=Naruto&limit=10&api_key=qsUmBiU0pSR30iCqtxxSlrEeVjYfJAGP`;
         const respuesta = await fetch(url);
         const {data} = await respuesta.json();
 
@@ -25,13 +26,23 @@ export const GifGrid = ({category}) => {
         })
 
         console.log(gifs);
+        setImages( gifs);
     }
 
+
+   
     
 
     return (
         <div>
             <h3>{category}</h3>
+            <ol>
+                {
+                    images.map(({id,title})=>
+                         <li key={id}>{title}</li>
+                    )
+                }
+            </ol>
         </div>
     )
 }
