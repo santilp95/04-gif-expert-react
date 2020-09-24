f092c63ad03c833a3679c1ec8fc8a1688766c79f
Santiago Lopez <santiagoloepz@gmail.com>
2020-09-23 20:43:38 -0500

    6.3 pruebas con mock

--- src/tests/components/GifGrid.test.js
+++ src/tests/components/GifGrid.test.js
@@ -2,12 +2,45 @@ import'@testing-library/jest-dom';
 import React from 'react';
 import { shallow } from 'enzyme';
 import { GifGrid } from '../../components/GifGrid';
+import { useFetechGifs } from '../../hooks/useFetechGifs';
+
+// con esto fingir cualquier llamada a este archivo y controlar la informacion
+jest.mock('../../hooks/useFetechGifs')
+
 describe('Pruebas componentes <GifGrid />', () => {
     const category = 'luna';
-    let wrapper = shallow(<GifGrid category={category}/>);
+    
 
     test('debe de mostrar <GifGridItem/> correctamente', () => {
+        useFetechGifs.mockReturnValue({
+            data:[],
+            loading: true
+        });
+
+        const wrapper = shallow(<GifGrid category={category}/>);
+
         expect(wrapper).toMatchSnapshot();
     });
+
+    test('debe de mostrar items cuando se cargar imagenes useFetchGifs', () => {
+        
+        const gifs = [{
+            id: 'ABC',
+            title: 'cualquier cosa',
+            url: 'https://localhost/cualquier.jpg'
+        }]
+        
+        useFetechGifs.mockReturnValue({
+            data:gifs,
+            loading: false
+        });
+
+        const wrapper = shallow(<GifGrid category={category}/>);
+
+        expect(wrapper).toMatchSnapshot();
+
+        
+    });
+    
     
 });
