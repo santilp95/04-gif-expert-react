04b8b94e725094be8a98bac0d78e319f43a9b174
Santiago Lopez <santiagoloepz@gmail.com>
2020-09-25 01:08:09 -0500

    8.3 simular el hooks y llamar lo que carga

--- src/tests/hooks/useFetechGifs.test.js
+++ src/tests/hooks/useFetechGifs.test.js
@@ -3,16 +3,30 @@ const { useFetechGifs } = require("../../hooks/useFetechGifs");
 
 describe('pruebas en el hook useFetechGifs', () => {
 
-    test('debe de retorinar el estado incinal', () => {
+    test('debe de retorinar el estado incinal', async() => {
         //esto srive para simular un componente para la prueba de los hooks
-        const {result} = renderHook(()=>useFetechGifs('Naruto'));
+        const {result,waitForNextUpdate} = renderHook(()=>useFetechGifs('Naruto'));
         const {data, loading} = result.current;
-
+        
+        await waitForNextUpdate();
         //const {data:images, loading} = useFetechGifs('Naruto');
 
         expect(data).toEqual([]);
         expect(loading).toBe(true);
         
     });
+
+    test('debe de retornar un arreglo de imgs y el loading en false', async() => {
+        // wait_< funcion que regresa una promesa que indica cuando sucede el cambio de resultado en el customhook
+        
+        const {result,waitForNextUpdate} = renderHook(()=>useFetechGifs('Naruto'));
+        await waitForNextUpdate();
+
+        const {data, loading} = result.current;
+        
+        expect(data.length).toEqual(10);
+        expect(loading).toBe(false);
+    });
+    
     
 });
